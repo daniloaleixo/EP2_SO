e0ef4d6977e61f96a7654152afb76017a200eca0
Danilo Aleixo Gomes de Souza <daniloaleixo@python.linux.ime.usp.br>
2016-09-15 10:41:24 -0300

    um pequeno passo para um homem um grande passo para o EP2

--- ep2.c
+++ ep2.c
@@ -20,19 +20,45 @@
 
 #define TRUE 1
 #define FALSE 0
-#define NUMERO_ENTRADAS 4
+#define NUMERO_ENTRADAS 2
+
+
+typedef struct nome {
+
+}Nome;
+
 
 
 int main(int argc, char *argv[])
 {
+  int d, n, v_ou_u;
+  char *nome_saida = "saida.txt"; FILE *arquivo_saida;
+  Nome *pista;
+  pthread_t *ciclistas;
 
-  if(argc >= NUMERO_ENTRADAS)
+
+  /* Pegamos as entradas do programa */
+  if(argc >= NUMERO_ENTRADAS + 1)
   {
-
+    d = atoi(argv[1]);
+    n = atoi(argv[2]);
+    if(argc == NUMERO_ENTRADAS + 2) v_ou_u = atoi(argv[3]);
   }
   else
     printf("Argumentos incorretos. Modo de utilizacao:\n\n"
-           "\tep2\n\n");
+           "\tep2  d  n   [v|u]\n\n");
+
+
+  /* aloca os vetores que vamos precisar */
+  pista = mallocSafe(d *  sizeof(Nome));
+  threads = malloc_safe(n * sizeof(pthread_t));
+
+  /* cria aquivo de saida */
+  arquivo_saida = cria_arquivo(nome_saida);
+
+
+
+
 
   return 0;
 }
