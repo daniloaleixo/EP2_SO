8f926db5fab3b95bcd73122be33d807d07383dd8
Danilo Aleixo <danilo_aleixo@hotmail.com>
2016-10-16 20:12:32 -0200

    Testes rodados

--- rodar_testes.sh
+++ rodar_testes.sh
@@ -17,31 +17,31 @@ done
 
 for i in $(seq 1 30)
 do
-  /usr/bin/time -v ./ep2 250 30 &> saidas/saida_0_250_$i
+  /usr/bin/time -v ./ep2 250 30 &> saidas/saida_30_250_$i
 done
 
 for i in $(seq 1 30)
 do
-  /usr/bin/time -v ./ep2 1000 30 &> saidas/saida_0_1000_$i
+  /usr/bin/time -v ./ep2 1000 30 &> saidas/saida_30_1000_$i
 done
 
 for i in $(seq 1 30)
 do
-  /usr/bin/time -v ./ep2 5000 30 &> saidas/saida_0_5000_$i
+  /usr/bin/time -v ./ep2 5000 30 &> saidas/saida_30_5000_$i
 done
 
 for i in $(seq 1 30)
 do
-  /usr/bin/time -v ./ep2 250 60 &> saidas/saida_0_250_$i
+  /usr/bin/time -v ./ep2 250 60 &> saidas/saida_60_250_$i
 done
 
 for i in $(seq 1 30)
 do
-  /usr/bin/time -v ./ep2 1000 60 &> saidas/saida_0_1000_$i
+  /usr/bin/time -v ./ep2 1000 60 &> saidas/saida_60_1000_$i
 done
 
 for i in $(seq 1 30)
 do
-  /usr/bin/time -v ./ep2 5000 60 &> saidas/saida_0_5000_$i
+  /usr/bin/time -v ./ep2 5000 60 &> saidas/saida_60_5000_$i
 done
 
