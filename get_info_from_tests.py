61c474060cf7a86ef7d8bc8fb59566b936312d05
Danilo Aleixo <danilo_aleixo@hotmail.com>
2016-10-16 18:01:57 -0200

    Funcao de fazer os testes esta feita vou colocar pra rodar

--- get_info_from_tests.py
+++ get_info_from_tests.py
@@ -1,14 +1,18 @@
 import sys
 import os
 
-NUMERO_ENTRADAS = 1
+NUMERO_ENTRADAS = 2
 TAMANHO_CICLISTAS = { 'peq': '_5_', 'med': '_30_', 'gra':'_60_' }
 TAMANHO_PISTA = { 'peq': '_250_', 'med': '_1000_', 'gra':'_5000_' }
+NOME_ARQS_RESULTADOS = ['p_peq_c_peq', 'p_peq_c_med', 'p_peq_c_gra',
+						'p_med_c_peq', 'p_med_c_med', 'p_med_c_gra',
+						'p_gra_c_peq', 'p_gra_c_med', 'p_gra_c_gra']
 
 
 
-tempos = [[],[],[],[],[],[],[],[],[]]
-# print tempos
+tempos_CPU = [[],[],[],[],[],[],[],[],[]]
+page_faults = [[],[],[],[],[],[],[],[],[]]
+context_switches = [[],[],[],[],[],[],[],[],[]]
 
 # existem 9 combinacoes para pistas e ciclistas elas sao 
  # 0 - pista peq. e ciclista peq.
@@ -25,11 +29,12 @@ tempos = [[],[],[],[],[],[],[],[],[]]
 
 if len(sys.argv) < NUMERO_ENTRADAS + 1:
 	print "Modo de uso\n\n"
-	print "get_info_from_tests <diretorio onde estao as saidas> "
+	print "get_info_from_tests <diretorio onde estao as saidas> <diretorio onde ficarao os resultados>"
 	print "\n"
 	exit(0)
 
 diretorio_saidas = sys.argv[1]
+diretorio_resultados = sys.argv[2];
 #print diretorio_saidas
 
 arquivos_saida = os.listdir(diretorio_saidas)
@@ -59,12 +64,33 @@ for i in range(0, len(arquivos_saida)):
 
 	with open('saidas/' + arquivos_saida[i]) as f:
 	    content = f.readlines()
-	#print content[4]
+
+	# print content[12].split(' ')[-1]
+	# print content[14].split(' ')[-1]
 	#print content[4].split(' ')[-1]
 
-	tempos[indice].append(content[4].split(' ')[-1].replace("\n", ""))
+	tempos_CPU[indice].append(content[4].split(' ')[-1])
+	page_faults[indice].append(content[12].split(' ')[-1])
+	context_switches[indice].append(content[14].split(' ')[-1])
 	#print tempo
 
-print tempos
+#print tempos_CPU
+
+for i in range(0, len(NOME_ARQS_RESULTADOS)):
+	file_tempos = open(diretorio_resultados + NOME_ARQS_RESULTADOS[i] + ".txt", "w")
+	file_tempos.write("Uso de CPU: \n")
+	for j in range(0, len(tempos_CPU[i])):
+		file_tempos.write(tempos_CPU[i][j])
+
+	file_tempos.write("\nPage Faults: \n")
+	for j in range(0, len(page_faults[i])):
+		file_tempos.write(page_faults[i][j])
+
+	file_tempos.write("\n Involuntary Context Switches: \n")
+	for j in range(0, len(context_switches[i])):
+		file_tempos.write(context_switches[i][j])
+
+	file_tempos.close()
+
 
 
